1e63c4e7e3fba5d73790b513d7f987ff3520084f
Marlon Santos Constante <marlon.constante@gmail.com>
2014-12-26 09:39:57 -0200

    Configurações do mongodb no heroku

--- js/db/database.js
+++ js/db/database.js
@@ -2,7 +2,8 @@
  * Created by marlon on 17/12/14.
  */
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/isdralit');
+//mongoose.connect('mongodb://localhost/isdralit');
+mongoose.connect('mongodb://heroku_app32850838:20492jnu2i4mdb88bk8qhum11r@ds043220.mongolab.com:43220/heroku_app32850838');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erro de conexão:'));
