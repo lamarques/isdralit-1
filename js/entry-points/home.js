65c604a4b8160d9152371c1da71595d106418b5b
Marlon Santos Constante <marlon.constante@gmail.com>
2014-12-30 16:59:02 -0200

    Extendendo base.ViewModel e adicionando camada específica

--- js/entry-points/home.js
+++ js/entry-points/home.js
@@ -4,4 +4,25 @@
 var libs = require('../common/libs');
 var base = require('../common/base');
 
-libs.ko.applyBindings(new base.ViewModel());
+ViewModel = function () {
+    var self = this;
+
+    self.banners = libs.ko.observableArray([]);
+
+    self.findBanners = function () {
+        libs.$.ajax({
+            url: '/banner/find'
+        }).done(function (banners) {
+            banners.forEach(function (banner) {
+                banner.backgroundImage = 'url(' + banner.imageUrl + ')';
+                self.banners.push(banner);
+            });
+        });
+    };
+
+    self.findBanners();
+
+    libs.ko.utils.extend(self, new base.ViewModel());
+};
+
+libs.ko.applyBindings(new ViewModel());
