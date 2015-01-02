c2dac72a046410ce0c5c7df5fb1af22524fe9917
Marlon Santos Constante <marlon.constante@gmail.com>
2015-01-02 10:15:31 -0200

    Script de carga inicial dos dados (mongodb)

---
+++ js/db/initial-charge.js
@@ -0,0 +1,14 @@
+/**
+ * Created by marlon on 30/12/14.
+ */
+var db = require('./database');
+
+var charge = function(entity) {
+    db.removeAll(entity.Model);
+    db.saveAll(entity.defaultValues);
+};
+
+exports.start = function () {
+    charge(db.Menu);
+    charge(db.Banner);
+};
