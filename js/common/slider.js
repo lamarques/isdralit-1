18a74037e4e062ac69060cdc8c6e2d4174c61736
Marlon Santos Constante <marlon.constante@gmail.com>
2015-01-06 13:44:25 -0200

    Adicionando script para inicializar o slider

---
+++ js/common/slider.js
@@ -0,0 +1,30 @@
+/**
+ * Created by marlon on 05/01/15.
+ */
+require('unslider');
+
+exports.init = function (selector) {
+    var $element = $(selector);
+    var unslider = $element.unslider({
+        delay: 10000,
+        dots: true
+    });
+
+    var data = unslider.data('unslider');
+
+    $element.find('.arrow.previous').click(function (event) {
+        data.prev();
+        event.stopPropagation();
+    });
+
+    $element.find('.arrow.next').click(function (event) {
+        data.next();
+        event.stopPropagation();
+    });
+
+    $element.click(function () {
+        var index = Number($(this).find('.dot.active').text()) - 1;
+        var $li = $(this).find('ul li:eq(' + index + ')');
+        window.location = $li.data('url');
+    });
+};
