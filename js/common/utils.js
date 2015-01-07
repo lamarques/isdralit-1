aef37c425fd29bc404a72cf1aaace6def0530800
Marlon Santos Constante <marlon.constante@gmail.com>
2015-01-07 17:07:49 -0200

    Adicionando utilitário para remover os atributos inválidos

--- js/common/utils.js
+++ js/common/utils.js
@@ -1,6 +1,14 @@
 /**
  * Created by marlon on 02/01/15.
  */
-exports.capitalize = function(text) {
+exports.capitalize = function (text) {
     return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+};
+
+exports.removeInvalidAttributes = function (data) {
+    for (var fieldName in data) {
+        if (!data[fieldName]) {
+            delete data[fieldName];
+        }
+    }
 };
