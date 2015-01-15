e8fdb0b3d3d18723372b47712974b8256e79f1fa
Marlon Santos Constante <marlon.constante@gmail.com>
2015-01-15 17:41:11 -0200

    Adicionando medidas e informações técnicas

--- js/db/product.js
+++ js/db/product.js
@@ -3,12 +3,28 @@
  */
 var mongoose = require('mongoose');
 
+MeasureSchema = mongoose.Schema({
+    extension: Number,
+    weight: Number,
+    width: Number,
+    thickness: Number,
+    order: Number
+});
+
+InformationSchema = mongoose.Schema({
+    titleHtml: String,
+    detailHtml: String,
+    order: Number
+});
+
 ItemSchema = mongoose.Schema({
     titleHtml: String,
     detailHtml: String,
     imageUrl: String,
     imagesUrl: Array,
-    order: Number
+    order: Number,
+    informations: [InformationSchema],
+    measures: [MeasureSchema]
 });
 
 Product = mongoose.model('Product', mongoose.Schema({
@@ -29,14 +45,118 @@ exports.defaultValues = [
                 detailHtml: 'Detalhes do produto 1',
                 imageUrl: '/images/tile.jpg',
                 imagesUrl: ['/images/tile-1.jpg', '/images/tile-2.jpg', '/images/tile-3.jpg', '/images/tile-4.jpg'],
-                order: 0
+                order: 0,
+                informations: [
+                    {
+                        titleHtml: 'Título 1',
+                        detailHtml: 'Recomendação 1',
+                        order: 0
+                    },
+                    {
+                        titleHtml: 'Título 2',
+                        detailHtml: 'Recomendação 2',
+                        order: 1
+                    },
+                    {
+                        titleHtml: 'Título 3',
+                        detailHtml: 'Recomendação 3',
+                        order: 2
+                    },
+                    {
+                        titleHtml: 'Título 4',
+                        detailHtml: 'Recomendação 4',
+                        order: 3
+                    }
+                ],
+                measures: [
+                    {
+                        extension: 1,
+                        weight: 2.25,
+                        width: 3.5,
+                        thickness: 4.75,
+                        order: 0
+                    },
+                    {
+                        extension: 1.5,
+                        weight: 3.25,
+                        width: 4,
+                        thickness: 5.25,
+                        order: 1
+                    },
+                    {
+                        extension: 2,
+                        weight: 4.25,
+                        width: 4.5,
+                        thickness: 5.75,
+                        order: 2
+                    },
+                    {
+                        extension: 2.5,
+                        weight: 5.25,
+                        width: 5,
+                        thickness: 6.25,
+                        order: 3
+                    }
+                ]
             },
             {
                 titleHtml: 'Item 2',
                 detailHtml: 'Detalhes do produto 2',
                 imageUrl: '/images/tile.jpg',
                 imagesUrl: ['/images/tile-1.jpg', '/images/tile-2.jpg', '/images/tile-3.jpg', '/images/tile-4.jpg'],
-                order: 1
+                order: 1,
+                informations: [
+                    {
+                        titleHtml: 'Título 1',
+                        detailHtml: 'Recomendação 1',
+                        order: 0
+                    },
+                    {
+                        titleHtml: 'Título 2',
+                        detailHtml: 'Recomendação 2',
+                        order: 1
+                    },
+                    {
+                        titleHtml: 'Título 3',
+                        detailHtml: 'Recomendação 3',
+                        order: 2
+                    },
+                    {
+                        titleHtml: 'Título 4',
+                        detailHtml: 'Recomendação 4',
+                        order: 3
+                    }
+                ],
+                measures: [
+                    {
+                        extension: 1,
+                        weight: 2.25,
+                        width: 3.5,
+                        thickness: 4.75,
+                        order: 0
+                    },
+                    {
+                        extension: 1.5,
+                        weight: 3.25,
+                        width: 4,
+                        thickness: 5.25,
+                        order: 1
+                    },
+                    {
+                        extension: 2,
+                        weight: 4.25,
+                        width: 4.5,
+                        thickness: 5.75,
+                        order: 2
+                    },
+                    {
+                        extension: 2.5,
+                        weight: 5.25,
+                        width: 5,
+                        thickness: 6.25,
+                        order: 3
+                    }
+                ]
             }
         ]
     }),
@@ -50,14 +170,118 @@ exports.defaultValues = [
                 detailHtml: 'Detalhes do produto 3',
                 imageUrl: '/images/tile.jpg',
                 imagesUrl: ['/images/tile-1.jpg', '/images/tile-2.jpg', '/images/tile-3.jpg', '/images/tile-4.jpg'],
-                order: 0
+                order: 0,
+                informations: [
+                    {
+                        titleHtml: 'Título 1',
+                        detailHtml: 'Recomendação 1',
+                        order: 0
+                    },
+                    {
+                        titleHtml: 'Título 2',
+                        detailHtml: 'Recomendação 2',
+                        order: 1
+                    },
+                    {
+                        titleHtml: 'Título 3',
+                        detailHtml: 'Recomendação 3',
+                        order: 2
+                    },
+                    {
+                        titleHtml: 'Título 4',
+                        detailHtml: 'Recomendação 4',
+                        order: 3
+                    }
+                ],
+                measures: [
+                    {
+                        extension: 1,
+                        weight: 2.25,
+                        width: 3.5,
+                        thickness: 4.75,
+                        order: 0
+                    },
+                    {
+                        extension: 1.5,
+                        weight: 3.25,
+                        width: 4,
+                        thickness: 5.25,
+                        order: 1
+                    },
+                    {
+                        extension: 2,
+                        weight: 4.25,
+                        width: 4.5,
+                        thickness: 5.75,
+                        order: 2
+                    },
+                    {
+                        extension: 2.5,
+                        weight: 5.25,
+                        width: 5,
+                        thickness: 6.25,
+                        order: 3
+                    }
+                ]
             },
             {
                 titleHtml: 'Item 4',
                 detailHtml: 'Detalhes do produto 4',
                 imageUrl: '/images/tile.jpg',
                 imagesUrl: ['/images/tile-1.jpg', '/images/tile-2.jpg', '/images/tile-3.jpg', '/images/tile-4.jpg'],
-                order: 1
+                order: 1,
+                informations: [
+                    {
+                        titleHtml: 'Título 1',
+                        detailHtml: 'Recomendação 1',
+                        order: 0
+                    },
+                    {
+                        titleHtml: 'Título 2',
+                        detailHtml: 'Recomendação 2',
+                        order: 1
+                    },
+                    {
+                        titleHtml: 'Título 3',
+                        detailHtml: 'Recomendação 3',
+                        order: 2
+                    },
+                    {
+                        titleHtml: 'Título 4',
+                        detailHtml: 'Recomendação 4',
+                        order: 3
+                    }
+                ],
+                measures: [
+                    {
+                        extension: 1,
+                        weight: 2.25,
+                        width: 3.5,
+                        thickness: 4.75,
+                        order: 0
+                    },
+                    {
+                        extension: 1.5,
+                        weight: 3.25,
+                        width: 4,
+                        thickness: 5.25,
+                        order: 1
+                    },
+                    {
+                        extension: 2,
+                        weight: 4.25,
+                        width: 4.5,
+                        thickness: 5.75,
+                        order: 2
+                    },
+                    {
+                        extension: 2.5,
+                        weight: 5.25,
+                        width: 5,
+                        thickness: 6.25,
+                        order: 3
+                    }
+                ]
             }
         ]
     }),
@@ -71,14 +295,118 @@ exports.defaultValues = [
                 detailHtml: 'Detalhes do produto 5',
                 imageUrl: '/images/tile.jpg',
                 imagesUrl: ['/images/tile-1.jpg', '/images/tile-2.jpg', '/images/tile-3.jpg', '/images/tile-4.jpg'],
-                order: 0
+                order: 0,
+                informations: [
+                    {
+                        titleHtml: 'Título 1',
+                        detailHtml: 'Recomendação 1',
+                        order: 0
+                    },
+                    {
+                        titleHtml: 'Título 2',
+                        detailHtml: 'Recomendação 2',
+                        order: 1
+                    },
+                    {
+                        titleHtml: 'Título 3',
+                        detailHtml: 'Recomendação 3',
+                        order: 2
+                    },
+                    {
+                        titleHtml: 'Título 4',
+                        detailHtml: 'Recomendação 4',
+                        order: 3
+                    }
+                ],
+                measures: [
+                    {
+                        extension: 1,
+                        weight: 2.25,
+                        width: 3.5,
+                        thickness: 4.75,
+                        order: 0
+                    },
+                    {
+                        extension: 1.5,
+                        weight: 3.25,
+                        width: 4,
+                        thickness: 5.25,
+                        order: 1
+                    },
+                    {
+                        extension: 2,
+                        weight: 4.25,
+                        width: 4.5,
+                        thickness: 5.75,
+                        order: 2
+                    },
+                    {
+                        extension: 2.5,
+                        weight: 5.25,
+                        width: 5,
+                        thickness: 6.25,
+                        order: 3
+                    }
+                ]
             },
             {
                 titleHtml: 'Item 6',
                 detailHtml: 'Detalhes do produto 6',
                 imageUrl: '/images/tile.jpg',
                 imagesUrl: ['/images/tile-1.jpg', '/images/tile-2.jpg', '/images/tile-3.jpg', '/images/tile-4.jpg'],
-                order: 1
+                order: 1,
+                informations: [
+                    {
+                        titleHtml: 'Título 1',
+                        detailHtml: 'Recomendação 1',
+                        order: 0
+                    },
+                    {
+                        titleHtml: 'Título 2',
+                        detailHtml: 'Recomendação 2',
+                        order: 1
+                    },
+                    {
+                        titleHtml: 'Título 3',
+                        detailHtml: 'Recomendação 3',
+                        order: 2
+                    },
+                    {
+                        titleHtml: 'Título 4',
+                        detailHtml: 'Recomendação 4',
+                        order: 3
+                    }
+                ],
+                measures: [
+                    {
+                        extension: 1,
+                        weight: 2.25,
+                        width: 3.5,
+                        thickness: 4.75,
+                        order: 0
+                    },
+                    {
+                        extension: 1.5,
+                        weight: 3.25,
+                        width: 4,
+                        thickness: 5.25,
+                        order: 1
+                    },
+                    {
+                        extension: 2,
+                        weight: 4.25,
+                        width: 4.5,
+                        thickness: 5.75,
+                        order: 2
+                    },
+                    {
+                        extension: 2.5,
+                        weight: 5.25,
+                        width: 5,
+                        thickness: 6.25,
+                        order: 3
+                    }
+                ]
             }
         ]
     }),
@@ -92,14 +420,118 @@ exports.defaultValues = [
                 detailHtml: 'Detalhes do produto 7',
                 imageUrl: '/images/tile.jpg',
                 imagesUrl: ['/images/tile-1.jpg', '/images/tile-2.jpg', '/images/tile-3.jpg', '/images/tile-4.jpg'],
-                order: 0
+                order: 0,
+                informations: [
+                    {
+                        titleHtml: 'Título 1',
+                        detailHtml: 'Recomendação 1',
+                        order: 0
+                    },
+                    {
+                        titleHtml: 'Título 2',
+                        detailHtml: 'Recomendação 2',
+                        order: 1
+                    },
+                    {
+                        titleHtml: 'Título 3',
+                        detailHtml: 'Recomendação 3',
+                        order: 2
+                    },
+                    {
+                        titleHtml: 'Título 4',
+                        detailHtml: 'Recomendação 4',
+                        order: 3
+                    }
+                ],
+                measures: [
+                    {
+                        extension: 1,
+                        weight: 2.25,
+                        width: 3.5,
+                        thickness: 4.75,
+                        order: 0
+                    },
+                    {
+                        extension: 1.5,
+                        weight: 3.25,
+                        width: 4,
+                        thickness: 5.25,
+                        order: 1
+                    },
+                    {
+                        extension: 2,
+                        weight: 4.25,
+                        width: 4.5,
+                        thickness: 5.75,
+                        order: 2
+                    },
+                    {
+                        extension: 2.5,
+                        weight: 5.25,
+                        width: 5,
+                        thickness: 6.25,
+                        order: 3
+                    }
+                ]
             },
             {
                 titleHtml: 'Item 8',
                 detailHtml: 'Detalhes do produto 8',
                 imageUrl: '/images/tile.jpg',
                 imagesUrl: ['/images/tile-1.jpg', '/images/tile-2.jpg', '/images/tile-3.jpg', '/images/tile-4.jpg'],
-                order: 1
+                order: 1,
+                informations: [
+                    {
+                        titleHtml: 'Título 1',
+                        detailHtml: 'Recomendação 1',
+                        order: 0
+                    },
+                    {
+                        titleHtml: 'Título 2',
+                        detailHtml: 'Recomendação 2',
+                        order: 1
+                    },
+                    {
+                        titleHtml: 'Título 3',
+                        detailHtml: 'Recomendação 3',
+                        order: 2
+                    },
+                    {
+                        titleHtml: 'Título 4',
+                        detailHtml: 'Recomendação 4',
+                        order: 3
+                    }
+                ],
+                measures: [
+                    {
+                        extension: 1,
+                        weight: 2.25,
+                        width: 3.5,
+                        thickness: 4.75,
+                        order: 0
+                    },
+                    {
+                        extension: 1.5,
+                        weight: 3.25,
+                        width: 4,
+                        thickness: 5.25,
+                        order: 1
+                    },
+                    {
+                        extension: 2,
+                        weight: 4.25,
+                        width: 4.5,
+                        thickness: 5.75,
+                        order: 2
+                    },
+                    {
+                        extension: 2.5,
+                        weight: 5.25,
+                        width: 5,
+                        thickness: 6.25,
+                        order: 3
+                    }
+                ]
             }
         ]
     })
