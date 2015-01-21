c273456c1554aa6d3a84b754d6078d9fde82edd0
Marlon Santos Constante <marlon.constante@gmail.com>
2015-01-21 17:50:33 -0200

    Alterando tela de exemplo (Dados de teste)

--- js/entry-points/admin/banner.js
+++ js/entry-points/admin/banner.js
@@ -10,6 +10,9 @@ require('bootstrap');
 ViewModel = function () {
     var self = this;
 
+    self.headers = ko.observableArray(['Coluna 1', 'Coluna 2']);
+    self.rows = ko.observableArray([['Valor 1', 'Valor 2'], ['Valor 3', 'Valor 4']]);
+
     ko.utils.extend(self, new base.ViewModel());
 };
 
