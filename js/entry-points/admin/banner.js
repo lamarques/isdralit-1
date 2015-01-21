0f41af253b370c61974cf72b0514c88d1cc4208b
Marlon Santos Constante <marlon.constante@gmail.com>
2015-01-21 07:46:57 -0200

    Adicionando css e js para a página administrativa (banner)

---
+++ js/entry-points/admin/banner.js
@@ -0,0 +1,14 @@
+/**
+ * Created by marlon on 21/01/15.
+ */
+var $ = require('jquery');
+var ko = require('knockout');
+var base = require('../../common/base');
+
+ViewModel = function () {
+    var self = this;
+
+    ko.utils.extend(self, new base.ViewModel());
+};
+
+ko.applyBindings(new ViewModel());
