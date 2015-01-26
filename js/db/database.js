86294f7cbad81ca5ad7984091d9740d3eb1c3fc5
Marlon Santos Constante <marlon.constante@gmail.com>
2015-01-26 08:28:09 -0200

    Adicionando funções saveOrUpdate e remove

--- js/db/database.js
+++ js/db/database.js
@@ -10,6 +10,18 @@ db.once('open', function () {
     console.log('Conexão realizada com sucesso');
 });
 
+exports.saveOrUpdate = function (model, data, callback) {
+    if (data._id) {
+        model.findByIdAndUpdate(data._id, data, callback);
+    } else {
+        new model(data).save(callback);
+    }
+};
+
+exports.remove = function (model, id, callback) {
+    model.findByIdAndRemove(id, callback);
+};
+
 exports.findAll = function (model, query, fields, callback) {
     model.find(query, fields).sort('order').lean().exec(callback);
 };
