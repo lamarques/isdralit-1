7604d20e70eb0e690f9b686cebcde89dde39b73a
Gustavo Henrique Medeiros de Castro <contato@castrohs.com>
2015-07-17 14:50:31 -0300

    tentativa4

--- js/db/item.js
+++ js/db/item.js
@@ -1,23 +1,34 @@
 
-/* global exports */
+/* global exports, ObjectId, ObjectId */
 
 var mongoose = require('mongoose');
 
-Item =  mongoose.model('Item', mongoose.Schema({
+var Item =  mongoose.model('Item', mongoose.Schema({
     key: String,
     titleHtml: String,
     detailHtml: String,
     backgroundImageUrl: String,
     imageUrl: String,
     imagesUrl: Array,
-    order: Number,
-    product: { type: mongoose.Schema.ObjectId, ref: 'Product' }
+    order: Number
+//    informations:  [{type: mongoose.Schema.ObjectId, ref:'InformationSchema'}],
+//    measures:  [{type: mongoose.Schema.ObjectId, ref:'MeasureSchema'}]  
 }));
+  
 
-exports.Model = Item;
   
-  
-  
+  exports.defaultValues = [
+    
+    new Item ({
+        key: 'caixas-de-agua-forte',
+        titleHtml: 'Caixas de água Forte',
+        detailHtml :'Caixas de água Forte',
+        imageUrl: '/images/water-tanks.jpg',
+        backgroundImageUrl:'/images/water-tanks.jpg',
+        order: 0
+        })
+];
+  exports.Model = Item;
 //
 //var InformationSchema = require('./InformationSchema.js');
 //var MeasureSchema = require('./MeasureSchema.js');
