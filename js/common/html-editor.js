8e6621f8f515952009801b8d06c152825e4aeecd
Marlon Santos Constante <marlon.constante@gmail.com>
2015-07-21 18:19:13 -0300

    Armazenando editor do textarea no elemento original

--- js/common/html-editor.js
+++ js/common/html-editor.js
@@ -6,10 +6,14 @@ require('uikit-html-editor');
 exports.init = function (field) {
     setTimeout(function () {
         var $element = $('#' + field.name);
+
         UIkit.htmleditor($element, {
             mode: 'tab',
             lblCodeview: 'HTML',
             lblPreview: 'Visualização'
         });
+
+        var editor = $element.parent().find('.CodeMirror')[0].CodeMirror;
+        $element.data('editor', editor);
     }, 0);
 };
