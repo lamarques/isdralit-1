50d888da99761b6704add176534ae10d1d0e142f
Marlon Santos Constante <marlon.constante@gmail.com>
2015-07-22 10:19:05 -0300

    Adicionando página de categoria

---
+++ js/entry-points/admin/category.js
@@ -0,0 +1,33 @@
+/**
+ * Created by marlon on 22/07/15.
+ */
+var $ = require('jquery');
+var ko = require('knockout');
+var crud = require('../../common/crud');
+
+ViewModel = function () {
+    var self = this;
+
+    self.dataModel = {
+        key: {
+            label: 'Chave da URL',
+            type: 'text'
+        },
+        titleHtml: {
+            label: 'Título',
+            type: 'html-editor'
+        },
+        image: {
+            label: 'Imagem',
+            type: 'upload'
+        },
+        order: {
+            label: 'Ordem',
+            type: 'number'
+        }
+    };
+
+    ko.utils.extend(self, new crud.ViewModel('banner', self.dataModel));
+};
+
+ko.applyBindings(new ViewModel());
