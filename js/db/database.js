a943263923ddee29494e7761066c87336dfa656f
Marlon Santos Constante <marlon.constante@gmail.com>
2015-07-23 13:54:12 -0300

    Habilitando carga de dados apenas em ambiente de desenvolvimento

--- js/db/database.js
+++ js/db/database.js
@@ -45,5 +45,7 @@ exports.Technical = require('./technical');
 exports.Product = require('./product');
 exports.Information = require('./information');
 
-var initialCharge = require('./initial-charge');
-initialCharge.start();
+if (process.env.NODE_ENV == 'development') {
+    var initialCharge = require('./initial-charge');
+    initialCharge.start();
+}
