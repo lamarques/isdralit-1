52a764b7da7a5e1c93b2ee2a829a3c2f5708d47f
Gustavo Henrique Medeiros de Castro <contato@castrohs.com>
2015-09-18 14:07:19 -0300

    formulário funcionando e salvando no banco

--- js/entry-points/admin/contact.js
+++ js/entry-points/admin/contact.js
@@ -1,31 +1,59 @@
-/**
- * Created by gustavocastrohs on 14/09/15.
- */
 
+//        category: ko.observable("Escolhe uma categoria")
+//    };
+//
+//    self.categories = [
+//        'Arquiteto', 'Construtor', 'Vendedor', 'Engenheiro', 'Consumidor final',
+//        'Profissional da Construção Civil', 'Lojista'
+//    ];
+//
+/**
+ * Created by marlon on 22/07/15.
+ */
 var $ = require('jquery');
 var ko = require('knockout');
-var utils = require('../../common/utils');
-var zoom = require('../../common/zoom');
 var crud = require('../../common/crud');
 
 ViewModel = function () {
     var self = this;
 
-    self.contact = {
-        firstName: ko.observable(),
-        lastName: ko.observable(),
-        email: ko.observable(),
-        subject: ko.observable(),
-        content: ko.observable(),
-        category: ko.observable()
+    self.dataModel = {
+
+        firstName: {
+            label: 'Nome',
+            type: 'text'
+        },
+        lastName: {
+            label: 'Sobrenome',
+            type: 'text'
+        },
+        email: {
+            label: 'E-mail',
+            type: 'text'
+        },
+        cellphone: {
+            label: 'Telefone',
+            type: 'text'
+        },
+        subject: {
+            label: 'Titulo',
+            type: 'text'
+        },
+        content: {
+            label: 'Sua menssagem',
+            type: 'text-area'
+        }
+        //,
+        //category: {
+        //    label: 'Categoria',
+        //    type: 'combo-box',
+        //    fieldOptionName: self.categories
+        //}
     };
-
-    self.categories = [
-        'Volvo', 'Saab', 'Fiat', 'Audi'
-    ];
-
-    //self.submit = function (data) {
-    //};
+    //self.categories = [
+    //    'Arquiteto', 'Construtor', 'Vendedor', 'Engenheiro', 'Consumidor final',
+    //    'Profissional da Construção Civil', 'Lojista'
+    //];
     ko.utils.extend(self, new crud.ViewModel('contact', self.dataModel));
 };
 
--- js/entry-points/site/contact.js
+++ js/entry-points/site/contact.js
@@ -1,34 +1,59 @@
+
+//        category: ko.observable("Escolhe uma categoria")
+//    };
+//
+//    self.categories = [
+//        'Arquiteto', 'Construtor', 'Vendedor', 'Engenheiro', 'Consumidor final',
+//        'Profissional da Construção Civil', 'Lojista'
+//    ];
+//
 /**
- * Created by marlon on 06/01/15.
+ * Created by marlon on 22/07/15.
  */
 var $ = require('jquery');
 var ko = require('knockout');
-var utils = require('../../common/utils');
-var zoom = require('../../common/zoom');
-var base = require('../../common/base');
 var crud = require('../../common/crud');
 
 ViewModel = function () {
     var self = this;
 
-    self.contact = {
-        firstName: ko.observable(),
-        lastName: ko.observable(),
-        email: ko.observable(),
-        subject: ko.observable(),
-        content: ko.observable(),
-        category: ko.observable()
+    self.dataModel = {
+
+        firstName: {
+            label: 'Nome',
+            type: 'text'
+        },
+        lastName: {
+            label: 'Sobrenome',
+            type: 'text'
+        },
+        email: {
+            label: 'E-mail',
+            type: 'text'
+        },
+        cellphone: {
+            label: 'Telefone',
+            type: 'text'
+        },
+        subject: {
+            label: 'Titulo',
+            type: 'text'
+        },
+        content: {
+            label: 'Sua menssagem',
+            type: 'text-area'
+        }
+        //,
+        //category: {
+        //    label: 'Categoria',
+        //    type: 'combo-box',
+        //    fieldOptionName: self.categories
+        //}
     };
-
-    self.categories = [
-        'Volvo', 'Saab', 'Fiat', 'Audi'
-    ];
-
-    self.submit = function (data) {
-    };
-
-
-    //ko.utils.extend(self, new base.ViewModel());
+    //self.categories = [
+    //    'Arquiteto', 'Construtor', 'Vendedor', 'Engenheiro', 'Consumidor final',
+    //    'Profissional da Construção Civil', 'Lojista'
+    //];
     ko.utils.extend(self, new crud.ViewModel('contact', self.dataModel));
 };
 
