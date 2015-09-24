b8bd6fffb3d6d55dfce6c25d36e497cd3bf791db
Gustavo Henrique Medeiros de Castro <contato@castrohs.com>
2015-09-24 14:58:03 -0300

    update do crud

--- js/common/crud.js
+++ js/common/crud.js
@@ -158,8 +158,17 @@ exports.ViewModel = function (name, dataModel) {
 
         self.fields().forEach(function (field) {
             var value = data[field.name];
+            if (!value && field.isMultiple)
+            {
+                value = [];
+            }
             field.value(external.findOption(field, value) || value);
         });
+       
+        if (!value && field.isMultiple)
+        {
+            value = [];
+        }
     };
 
     self.getValue = function (value) {
