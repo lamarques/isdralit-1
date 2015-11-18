824b4e36ab9c84b2d8c8022006d48781cbea53d1
Gustavo Henrique Medeiros de Castro <contato@castrohs.com>
2015-11-18 14:20:57 -0200

    update

--- js/entry-points/site/contact.js
+++ js/entry-points/site/contact.js
@@ -13,7 +13,8 @@
 var $ = require('jquery');
 var ko = require('knockout');
 var crud = require('../../common/crud');
-
+var now = new Date();
+console.log(now);
 ViewModel = function () {
     var self = this;
 
@@ -44,20 +45,19 @@ ViewModel = function () {
         content: {
             label: 'Sua mensagem',
             type: 'text-area'
+
         }
-        //states: ko.observableArray(['RS', 'SC', 'PR']),
-        //categories : ko.observableArray( [
-        //    'Arquiteto', 'Construtor', 'Vendedor', 'Engenheiro', 'Consumidor final',
-        //    'Profissional da Construção Civil', 'Lojista'
-        //])
+
+
+
+
     };
-    //var states= ko.observableArray(['RS', 'SC', 'PR']);
-    //self.firstName = ko.observable().extend({ required: true });
-    //if (self.firstName !=null)
+
+
+
         ko.utils.extend(self, new crud.ViewModelContact('contact', self.dataModel));
-    //else
-    //    ko.utils.extend(self, null);
 
 };
+
 
 ko.applyBindings(new ViewModel());
